3b63f975e404593385bbc37b1f226003e417b11f
Felipe Pichinine <felipe.pichinine@aluno.unip.br>
2023-01-06 16:11:07 -0300

    Add project files.

---
+++ AluraEstacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,24 @@
+using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace AluraEstacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Acelerar(10);
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Frear(10);
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
